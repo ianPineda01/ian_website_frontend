aedb3edae81726a7ac783fdf3e0681fa4d9fe703
ianPineda <ianpineda@ciencias.unam.mx>
2021-10-22 13:49:39 -0500

    Add Contact View

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About me</router-link> |
-    <router-link to="/snake">Snake</router-link>
+    <router-link to="/snake">Snake</router-link> |
+    <router-link to="/contact">Contact</router-link>
   </div>
   <router-view/>
 </template>
---
+++ src/views/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <a href="https://www.gitub.com/ianpineda01"><img src="../assets/github.png" alt="Github logo"></a>
+  <a href="mailto:ianpineda@ciencias.unam.mx"><img src="../assets/mail.png" alt="Mail logo"></a>
+  <a href="https://www.linkedin.com/in/ian-eduardo-bravo-pineda-ab7034209/"><img src="../assets/linkedin.png" alt="Linkedin logo"></a>
+  <a href="https://telegram.me/ianpineda01"><img src="../assets/telegram.png" alt="Telegram logo"></a>
+
+  <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+</template>
+<style scoped>
+img:hover{
+  fill: blue;
+}
+</style>
