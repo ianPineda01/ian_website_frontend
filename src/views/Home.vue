71aaccd1b915e3f0ef0e6f12412cacc745c3d661
ianPineda <ianpineda@ciencias.unam.mx>
2022-02-22 15:43:25 -0600

    Add tailwindcss logo to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,17 @@
   <div class="w-max mx-auto text-center">
     <h1 >Ian Pineda</h1>
     <h3>(Under construction)</h3>
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="flex flex-col md:flex-row">
+      <img alt="Vue logo" src="../assets/vue.png">
+      <img src="../assets/tailwind.svg" alt="Tailwind logo" width="200">
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
-  components: {
-    HelloWorld,
-  },
 })
 export default class Home extends Vue {}
 </script>
